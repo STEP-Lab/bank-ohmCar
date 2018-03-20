f1deb06a090f5d067d3e1927894b5aab1e2c269a
omkar mote <omkarmote210@gmail.com>
2018-03-20 16:41:51 +0530

    Added methods for throwing validating account balance and account number;

--- library/src/main/java/com/thoughtworks/step/Account.java
+++ library/src/main/java/com/thoughtworks/step/Account.java
@@ -6,31 +6,38 @@ public class Account {
 
     public Account(String accountNumber, int balance) throws MinimumBalanceException,InvalidAccountNumberException {
         this.accountNumber = accountNumber;
-        if(balance < 1000){
-            throw new MinimumBalanceException("Insufficient balance to create an account");
-        }
-        if(!accountNumber.matches("\\d{4}-\\d{4}")){
-            throw new InvalidAccountNumberException("Invalid account number");
-        }
+        validateBalance(balance,"Insufficient balance to create an account");
+        validateAccountNumber(accountNumber);
         this.balance = balance;
     }
 
+    private static void validateAccountNumber(String accountNumber) throws InvalidAccountNumberException {
+        if(!accountNumber.matches("\\d{4}-\\d{4}")){
+            throw new InvalidAccountNumberException("Invalid account number");
+        }
+    }
+
+    private static void validateBalance(int balance,String message) throws MinimumBalanceException {
+        if(balance < 1000){
+            throw new MinimumBalanceException(message);
+        }
+    }
+
+
     public int getBalance() {
-        return balance;
+        return this.balance;
     }
 
     public String getAccountNumber() {
-        return accountNumber;
+        return this.accountNumber;
     }
 
     public void credit(int amount) {
-        balance+=amount;
+        this.balance+=amount;
     }
 
     public void debit(int amount) throws MinimumBalanceException{
-        balance-=amount;
-        if(balance < 1000) {
-            throw new MinimumBalanceException("Can't process your withdraw request");
-        }
+        this.balance-=amount;
+        validateBalance(balance,"Can't process your withdraw request");
     }
 }
