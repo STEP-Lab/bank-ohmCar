e0a24b663865831c61341d28541f42a314ff9bab
omkar mote <omkarmote210@gmail.com>
2018-03-24 11:35:59 +0530

    Tested debit and credit transaction's record.

--- library/src/test/java/com/thoughtworks/step/TransactionsTest.java
+++ library/src/test/java/com/thoughtworks/step/TransactionsTest.java
@@ -4,16 +4,30 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
 
 public class TransactionsTest {
     @Test
-    public void recordTransaction() {
+    public void shouldRecordCreditTransaction() {
         Transactions transactions = new Transactions();
-        transactions.debit("Omkar",1000);
-        assertThat(transactions.allTransactions,hasItem(new DebitTransaction(new Date(),"Omkar",1000)));
-        transactions.credit("Ketan",1000);
-        assertThat(transactions.allTransactions,hasItem(new CreditTransaction(new Date(),"Ketan",1000)));
+        transactions.credit("Omkar",1000);
+        assertThat(transactions.allTransactions,hasItem(new CreditTransaction(new Date(),"Omkar",1000)));
+    }
+
+    @Test
+    public void shouldRecordDebitTransaction() {
+        Transactions transactions = new Transactions();
+        transactions.debit("Ketan",1000);
+        assertThat(transactions.allTransactions,hasItem(new DebitTransaction(new Date(),"Ketan",1000)));
+    }
+
+    @Test
+    public void shouldRecordAllTransactions() {
+        Transactions transactions = new Transactions();
+        transactions.credit("Omkar",1000);
+        transactions.debit("Ketan",1000);
+        assertThat(transactions.allTransactions,hasItems(new CreditTransaction(new Date(),"Omkar",1000),new DebitTransaction(new Date(),"Ketan",1000)));
     }
 }
